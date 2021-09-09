e2a76fc956e7c229d4ca5d2a4c7503f8592f8de9
ehsan <ehsansumra@gmail.com>
2021-09-09 12:14:05 -0400

    cartesian refactor

--- exercises/cartesian.py
+++ exercises/cartesian.py
@@ -8,9 +8,8 @@ def squareRoot(num):
     return num**0.5
     
 def computeCartesianDistance(x1, y1, x2, y2):
-    x = square(x2 - x1)
-    y = square(y2 - y1)
-    return squareRoot(x + y)
+    
+    return squareRoot(square(x2 - x1) + square(y2 - y1))
 
 if __name__ == "__main__":
     dist = computeCartesianDistance(-7, -4, 17, 6.5)
