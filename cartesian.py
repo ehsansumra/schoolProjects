8051207111dcbf56d3bcb67155eef231ff1143cc
ehsan <ehsansumra@gmail.com>
2021-09-09 12:11:06 -0400

    cartesian exercise

--- cartesian.py
+++ cartesian.py
@@ -1,11 +1,17 @@
 
 # TODO: divide into functions
-def computeCartesianDistance(x1, y1, x2, y2):
-    x = (x2 - x1)**2
-    y = (y2 - y1)**2
 
-    return (x + y)**0.5
+def square(num):
+    return num * num
+
+def squareRoot(num):
+    return num**0.5
+    
+def computeCartesianDistance(x1, y1, x2, y2):
+    x = square(x2 - x1)
+    y = square(y2 - y1)
+    return squareRoot(x + y)
 
 if __name__ == "__main__":
-    dist = computeCartesianDistance(0,5, 7, 8)
+    dist = computeCartesianDistance(-7, -4, 17, 6.5)
     print(dist)
